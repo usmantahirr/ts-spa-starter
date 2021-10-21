42e1ad957b316f8272087fdeb9e155440fd46ec0
Usman Tahir (P) <ch.usman.tahir@gmail.com>
2021-10-21 14:30:57 +0200

    TS Aliases complete

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = function override(config, env) {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src/"),
+      "@api": path.resolve(__dirname, "src/api/"),
+      "@configs": path.resolve(__dirname, "src/configs/"),
+      "@components": path.resolve(__dirname, "src/shared/components/"),
+      "@pages": path.resolve(__dirname, "src/shared/pages/"),
+      "@templates": path.resolve(__dirname, "src/shared/templates/"),
+      "@modules": path.resolve(__dirname, "src/shared/modules/"),
+      "@containers": path.resolve(__dirname, "src/containers/"),
+      "@store": path.resolve(__dirname, "src/store/"),
+      "@types": path.resolve(__dirname, "src/types/"),
+      "@utils": path.resolve(__dirname, "src/utils/"),
+    },
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".d.ts"],
+  };
+  return config;
+};
